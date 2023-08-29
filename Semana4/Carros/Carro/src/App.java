f44c3cf180730153db4456e6542d0176ea8567dc
Carlos Torres <carlos.torreslo@campusuc.edu.co>
2023-08-29 11:59:32 -0500

    Semana5

--- Semana4/Carros/Carro/src/App.java
+++ Semana4/Carros/Carro/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola!!!");
+        Carro cadilac = new Carro("Hyundai","#ffffff" , 120, 80, 500, 100, false, 50);
+        cadilac.mostrarInformacion();
+        cadilac.arrancar();
+        cadilac.mostrarInformacion();
+        cadilac.arrancar();
+        cadilac.acelerar(100);
+        cadilac.parar();
+        cadilac.repostar(250);
+        cadilac.arrancar();
+        cadilac.viaje(1, 2);
+        cadilac.mostrarInformacion();
+
     }
 }
--- Semana4/Carros/Carro/src/Carro.java
+++ Semana4/Carros/Carro/src/Carro.java
@@ -4,7 +4,7 @@ public class Carro{
     private float velocidadMaxima;
     private float velocidad;
     private float combustibleMaximo;
-    private float combstible;
+    private float combustible;
     private boolean estado = false;
     private float consumo;
 
@@ -15,15 +15,15 @@ public class Carro{
      * @param velocidadMaxima la velocidad debe ser mayor que 0 esta dada en km/h
      * @param velocidad es la velocidad al momento de crear el carro deberia ser 0 
      * @param combustibleMaximo es la capacidad del anque del carro en litros debe ser mayor 0
-     * @param combstible es la cantidad actual de combustible en el carro deberia ser 0 
+     * @param combustible es la cantidad actual de combustible en el carro deberia ser 0 
      * @param estado es true si el caro esta encendido y false si esta apagado
      * @param consumo es la relacion de km por litros que tiene el carro
      */
-    public Carro(String modelo, String color, float velocidadMaxima, float velocidad, float combustibleMaximo, float combstible, boolean estado, float consumo){
+    public Carro(String modelo, String color, float velocidadMaxima, float velocidad, float combustibleMaximo, float combustible, boolean estado, float consumo){
         this.modelo =  modelo;
         this.color =  color;
         this.combustibleMaximo = combustibleMaximo;
-        this.combstible =  combstible;
+        this.combustible =  combustible;
         this.velocidadMaxima =  velocidadMaxima;
         this.velocidad =  velocidad;
         this.estado = estado;
@@ -41,50 +41,54 @@ public class Carro{
         this.modelo =  "";
         this.color =  "";
         this.combustibleMaximo = 0;
-        this.combstible =  0;
+        this.combustible =  0;
         this.velocidadMaxima =  0;
         this.velocidad =  0;
         this.estado = false;
         this.consumo = 0;
     }
 
-
-
-    
     /**
-     * 
-     * @param tiempo
-     * @param velocidad
-     * @return
+     * arrancar no activa el carro si esta pagado
+     * @return verdadero si el carro se encendio, falso en caso contrario
      */
-    public boolean viaje(float tiempo, float velocidad){
-
-        return true;
+    public boolean arrancar(){
+        boolean resultado = true;
+        if (isEstado()){
+            resultado =  false;
+            System.out.println("El carro ya esta encendido, no se puede volver a encender");
+        }else{
+            if (getVelocidad()>0){
+                resultado =  false;
+                setVelocidad(0);
+                System.out.println("La velocidad del carro no era 0 se ha puesto a 0 vuelva a intentar arrancarlo");
+            }else{
+                setEstado(resultado);
+                System.out.println("Carro encendido");
+            }
+            
+        }
+        return resultado;
     }
 
-    public String getModelo() {
-        return modelo;
+    /**
+     * parar: pasa el estado del carro a apagado y velocidad a cero
+     * @return erdadero si el carro se apago, falso en caso contrario
+     */
+    public boolean parar(){
+        boolean resultado = false;
+        if (isEstado()){
+            setEstado(false);
+            setVelocidad(0);
+            System.out.println("Carro apagado");
+            resultado = true;
+        }else{
+            System.out.println("El carro ya esta apagado, no se puede volver a apagar");
+            resultado = false;
+        }
+        return resultado;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public float getVelocidadMaxima() {
-        return velocidadMaxima;
-    }
-
-    public float getVelocidad() {
-        return velocidad;
-    }
 
     /**
      * acelerar --> Cambia la velocidad
@@ -122,17 +126,97 @@ public class Carro{
         return resultado;
     }
 
+    public boolean repostar(float combustible){
+        boolean resultado = false;
+        if (isEstado()== false){
+            if (combustible <= (getCombustibleMaximo()-getCombustible())){
+                setCombustible(combustible+getCombustible());
+                System.out.println("Repostaje exitoso");
+                resultado =  true;
+            } else{
+                System.out.println("Cantidad de combustible es superior a la permitida ");
+            }
+        } else{
+            System.out.println("El carro debe esta apagado para repostar");
+        }
+        return resultado;
+    }
+    
+    /**
+     * 
+     * @param tiempo
+     * @param velocidad
+     * @return
+     */
+    public boolean viaje(float tiempo, float velocidad){
+        boolean resultado = false;
+        float consumoViaje, distancia;
+        if (isEstado()){
+            distancia = tiempo * velocidad;
+            consumoViaje = getConsumo()*distancia;
+            if (consumoViaje<=getCombustible()){
+                setVelocidad(velocidad);
+                setCombustible(getCombustible()-consumoViaje);
+                System.out.println("Vije realizado con exito");
+                resultado = true;
+            } else {
+                System.out.println("el combustible actual no permite realizar el viaje");
+            } 
+        }else{
+            System.out.println("El carro debe estar encendido");
+        }
+        return resultado;
+    }
+
+    /**
+     * mostrarInformacion hace una impresion por pantalla de los datos del Carro
+     * @return verdadero si imprime
+     */
+    public boolean mostrarInformacion(){
+        boolean resultado = true;
+        System.out.println(toString());
+        return resultado;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public float getVelocidad() {
+        return velocidad;
+    }
+ 
+    public void setVelocidad(float velocidad) {
+        this.velocidad = velocidad;
+    }
+    
     public float getCombustibleMaximo() {
         return combustibleMaximo;
     }
 
   
-    public float getCombstible() {
-        return combstible;
+    public float getCombustible() {
+        return combustible;
     }
 
-    public void setCombstible(float combstible) {
-        this.combstible = combstible;
+    public void setCombustible(float combustible) {
+        this.combustible = combustible;
     }
 
     public boolean isEstado() {
@@ -146,6 +230,18 @@ public class Carro{
     public float getConsumo() {
         return consumo;
     }
+
+    @Override
+    public String toString() {
+        return "Carro [modelo=" + modelo + ", color=" + color + ", velocidadMaxima=" + velocidadMaxima + ", velocidad="
+                + velocidad + ", combustibleMaximo=" + combustibleMaximo + ", combustible=" + combustible + ", estado="
+                + estado + ", consumo=" + consumo + "]";
+    }
+
+   
+
+   
+
 
     
 }
